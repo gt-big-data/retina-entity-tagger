da7d7f1d83bdcc9a83771392b23ad3c9851430a4
Neil Goel <Neil@lawn-128-61-50-36.lawn.gatech.edu>
2015-09-04 19:02:46 -0400

    updated run entities

--- runEntities.sh
+++ runEntities.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/entity/retina-entity-tagger
 echo $(date) >> entityRuns.log
-python -m retina-entity-tagger.1-nameEntityExtraction && echo $(date) >> entitySuccess.log
+python -m 1-nameEntityExtraction && echo $(date) >> entitySuccess.log
