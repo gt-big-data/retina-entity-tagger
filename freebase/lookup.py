ca8c6351342b7986e4f6f2bc5f14eca29435eb01
Akhilesh Aji <akhiaji@gmail.com>
2015-09-05 10:08:59 -0400

    replaced urllib with requests, and modified params to generate better results

--- freebase/lookup.py
+++ freebase/lookup.py
@@ -1,14 +1,17 @@
 import json
-import urllib
+#import pprint
+import requests
 
 def lookup(name):
     service_url = 'https://www.googleapis.com/freebase/v1/search'
     params = {
-            "id": None,
-            "name": name,
-            "type": "/location/location",
-            "/location/location/contains": []
-        }
-    url = service_url + '?' + urllib.urlencode(params)
-    response = json.loads(urllib.urlopen(url).read())
+              "id": None,
+              "query": "atlanta",
+              "type": "/location/location",
+              "/location/location/containedby": []
+            }
+    request = requests.get(service_url, params = params)
+    response = json.loads(request.text)
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(response['result'])
     return response['result']
