e0d500aa359674f32bae429bf8ba48914041bda5
= <=>
2015-09-21 18:36:58 -0400

    changed urlib to request and updated query

--- freebase/lookup.py
+++ freebase/lookup.py
@@ -6,12 +6,13 @@ def lookup(name):
     service_url = 'https://www.googleapis.com/freebase/v1/search'
     params = {
               "id": None,
-              "query": "atlanta",
+              "query": name,
               "type": "/location/location",
               "/location/location/containedby": []
             }
     request = requests.get(service_url, params = params)
     response = json.loads(request.text)
-    # pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(response['result'])
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(response['result'])
     return response['result']
+
