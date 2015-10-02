15f08dd651d79fb786e1c576c6725ec15f58b220
Osama Sakhi <msakhi21@gmail.com>
2015-10-02 01:20:45 -0400

    Works with unicode strings. Saves highlight sentences with corresponding matching entities

--- entity_highlights.py
+++ entity_highlights.py
@@ -1,22 +1,13 @@
 from dbco import *
 from nltk.tokenize import sent_tokenize
+import re
 
 def create_sentences(content):
 	'''Divide content into sentences'''
 	sentences = sent_tokenize(content)
-	valid = []
-	for sentence in sentences:
-		
-		try:
-			# Decodes sentences names so python can work with them, removes newline characters
-			sentence = str(sentence).strip()
-			valid.append(sentence)
-
-			# If the sentence has some strange unicode character in it, we'll ignore it for now
-		except UnicodeEncodeError:
-			pass
-
-	return valid
+	# Remove newline character from sentences, along with preceding or trailing whitespaces
+	sentences = [re.sub('\n', '', sentence).strip() for sentence in sentences]
+	return sentences
 
 def get_highlighted_sentences(sentences, entities):
 	'''Takes in paragraphs of text and a list of entities, returns a list of the sentences in content in which any number of words in entitites shows'''
@@ -37,36 +28,36 @@ def get_highlighted_sentences(sentences, entities):
 			'sentence': sentence,
 			'entitites': matching_entities
 			}
-			print highlight
-			highlights.append(highlights)
+			highlights.append(highlight)
 
 	return highlights
 
-def save_to_db(result, highlights):
-	doc_id = result["_id"]
-	print doc_id
-	# print highlights
-	# db.qdoc.update( { "_id": doc_id },{"$set": {"highlights": highlights} } )
+def save_to_db(article, highlights):
+	'''Updates an article in the db to include the highlights'''
+	doc_id = article["_id"]
+	db.qdoc.update( { "_id": doc_id },{"$set": {"highlights": highlights} } )
 
 
-# Gets results where we have entities
-def main():
-	limit = 10
-	results = db.qdoc.find({ "$query": { "entities": { "$exists": True }, "content": { "$exists": True } }, "$orderby": { '_id' : -1 } },
+def main(limit=10):
+	'''Fetches some number of articles, and saves to the databases the sentences that have a match with one of our entitites for the given article'''
+
+	# Gets articles where we have entities and content, sort by how recent article is
+	articles = db.qdoc.find({ "$query": { "entities": { "$exists": True }, "content": { "$exists": True } },
+		"$orderby": { '_id' : -1 } },
 		{ "content": 1, "entities": 1}).limit(limit)
 
-	for result in results:
+	# Processes each article one at a time
+	for article in articles:
 
-		result = dict([(k.encode('utf-8'), v) for k, v in result.iteritems()])
-		content = result['content']
-		entities = result['entities']
-		# Decodes entity names so python can work with them
-		entities = [entity.decode('utf-8') for entity in entities]
+		# Converts key names to python strings so lookup is convenient
+		article = dict([(k.encode('utf-8'), v) for k, v in article.iteritems()])
+		content = article['content']
+		entities = article['entities']
 
 		sentences = create_sentences(content)
 		highlights = get_highlighted_sentences(sentences, entities)
 
-		save_to_db(result, highlights)
+		save_to_db(article, highlights)
 
 
 if __name__ == "__main__":
