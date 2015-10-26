c669511d7a40cedcfd3b74755df750f1f9c25b93
Neil Goel <neil9518@gmail.com>
2015-10-26 19:56:32 -0400

    updates entities to wikidata ids

--- update_entity_ids.py
+++ update_entity_ids.py
@@ -2,19 +2,20 @@ from dbco import *
 from name_entity_extraction import *
 
 #updates entities to wikidata ids
-def updateEntities(limit=1000):
-    articles = db.qdoc.find({ "$query": { "entities": { "$exists": True } }, "$orderby": { '_id' : 1 } }).limit(limit)
+def updateEntities():
+    articles = db.qdoc.find({ "$query": { "entities": { "$exists": True } }, "$orderby": { '_id' : 1 } })
     for a in articles:
         entities = a['entities']
         updated = isUpdated(entities)
         if not updated:
+            #print entities
             newEntities = lookupNamedEntities(entities)
+            #print newEntities
             db.qdoc.update( { "_id": a['_id'] }, { "$set": {"entities": newEntities} } )
-            
+            #print "---------------------------"
 
 #determines if entities need to be updated
 def isUpdated(entities):
-    
     numOfQs = 0
     for e in entities:
         if e is None:
@@ -22,17 +23,20 @@ def isUpdated(entities):
         elif e[0:1] == 'Q':
             numOfQs += 1
 
-    if len(entities) is numOfQs:
+    if len(entities) == numOfQs:
         return True
     else:
+        #print numOfQs
+        #print len(entities)
         return False
 
 #delete entities1 field
 def deleteEntities1():
-    db.qdoc.update(
-    { "entities1": {"$exists" : True} },
-    { "$unset": {"entities1" : True} },
-    False, True
-    )
+    bulk = db.qdoc.initialize_unordered_bulk_op()
+    bulk.find({"entities1": {"$exists" : True} }).update({ "$unset": {"entities1" : True} })
+    result = bulk.execute()
+    print(result)
 
-#updateEntities(100)
+updateEntities()
+#print db.qdoc.count()
+#print db.qdoc.find({"entities1": {"$exists":True}}).count()
