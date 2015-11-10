2a8e88f0d4ef8ae0ebf166c02ab809fdba21b3ad
Sam Yeager <samyeager@netscape.net>
2015-11-09 20:08:40 -0500

    Updated getNameEntities with spacy, and tagEntities to remove articles with no content.

--- name_entity_extraction.py
+++ name_entity_extraction.py
@@ -1,3 +1,4 @@
+import name_entity_spacy as spcy
 import nltk
 from wikidata import WikidataEntityLookup
 from dbco import *
@@ -6,7 +7,7 @@ wd = WikidataEntityLookup()
 def lookupNamedEntities(namedEntityTexts):
     '''
     Given list of texts that correspond to named entities,
-    return a list of dictionaries, where each dict has
+    return a list of dictionaries, where each dict has 
     the original text, entity id, and description.
 
     Example usage:
@@ -16,35 +17,39 @@ def lookupNamedEntities(namedEntityTexts):
         {'text': 'New York State', 'id': 'Q1380', 'description': 'state in us..'}, ..
     ]
     '''
-    returned_list = []
-
+    returned_list = []    
+    
     for i in xrange(len(namedEntityTexts)):
         entity = namedEntityTexts[i]
         entityId = wd.searchEntities(entity)
         returned_list.append(entityId)
-
+        
     return returned_list
 
 def getNameEntities(text):
-    sentences = nltk.sent_tokenize(text)
-    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+    
+### BELOW IS COMMENTED TO TEST THE SPACY METHOD
+#    sentences = nltk.sent_tokenize(text)
+#    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+#    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+#    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
 
-    nameEntity = []
-    for sentence in chunked_sentences:
-        for part in sentence:
-            if type(part) is nltk.Tree:
-                entityTree = part.leaves()
-                entity = ""
-                for tuplePair in entityTree:
-                    entity += " "
-                    entity += tuplePair[0]
-                entity = entity[1:]
-                nameEntity.append(entity)
-    nameEntity = list(set(nameEntity))
-    entities = lookupNamedEntities(nameEntity)
-    return entities
+#    nameEntity = []
+#    for sentence in chunked_sentences:
+#        for part in sentence:
+#            if type(part) is nltk.Tree:
+#                entityTree = part.leaves()
+#                entity = ""
+#                for tuplePair in entityTree:
+#                    entity += " "
+#                    entity += tuplePair[0]
+#                entity = entity[1:]
+#                nameEntity.append(entity)
+#    nameEntity = list(set(nameEntity))
+    
+    nameEntity = spcy.get_entities_spacy(text) # THE SPACY METHOD
+
+    return lookupNamedEntities(nameEntity)
 
 # def entityTester():
 #     with open("small_sample_articles.json") as f:
@@ -59,7 +64,12 @@ def getArticlesNoEntities(limit=1000):
 def tagEntities():
     articles = getArticlesNoEntities()
     for a in articles:
-        db.qdoc.update( { "_id": a['_id'] },{"$set": {"entities": getNameEntities(a['content'] ) } } )
+        try:
+            db.qdoc.update( { "_id": a['_id'] },{"$set": {"entities": getNameEntities(a['content'] ) } } )
+        except:
+            err_str = 'No content in article {id}. Deleting article.'.format(id = a['_id'])
+            print(err_str)
+            db.qdoc.remove(spec_or_id = a['_id'])
 
 if __name__ == "__main__":
     tagEntities()
