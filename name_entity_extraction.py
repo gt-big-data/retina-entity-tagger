d71f657d3d0980bbab5ab88102e92e37f2a19088
Patrick Violette <violette.patrick718@gmail.com>
2015-11-13 18:18:52 -0500

    Update spacy to save more data

--- name_entity_extraction.py
+++ name_entity_extraction.py
@@ -29,35 +29,6 @@ def lookupNamedEntities(namedEntityTexts):
         
     return returned_list
 
-def getNameEntities(text):
-    
-### BELOW IS COMMENTED TO TEST THE SPACY METHOD
-#    sentences = nltk.sent_tokenize(text)
-#    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-#    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-#    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
-
-#    nameEntity = []
-#    for sentence in chunked_sentences:
-#        for part in sentence:
-#            if type(part) is nltk.Tree:
-#                entityTree = part.leaves()
-#                entity = ""
-#                for tuplePair in entityTree:
-#                    entity += " "
-#                    entity += tuplePair[0]
-#                entity = entity[1:]
-#                nameEntity.append(entity)
-#    nameEntity = list(set(nameEntity))
-    
-    nameEntity = spcy.get_entities_spacy(text) # THE SPACY METHOD
-
-    return lookupNamedEntities(nameEntity)
-
-# def entityTester():
-#     with open("small_sample_articles.json") as f:
-#         for line in f:
-#             print(getNameEntities(json.loads(line)["content"]))
 
 def getArticlesNoEntities(limit=1000):
     articles = db.qdoc.find({ "$query": { "entities": { "$exists": False } }, "$orderby": { '_id' : -1 } }).limit(limit)
@@ -68,7 +39,10 @@ def tagEntities():
     articles = getArticlesNoEntities()
     for a in articles:
         try:
-            db.qdoc.update( { "_id": a['_id'] },{"$set": {"entities": getNameEntities(a['content'] ) } } )
+            parsed_text = spcy.nlp_parse(a['content'])
+            entities = lookupNamedEntities(parsed_text['entities'])
+            unique_entities = sorted(set([entity for entity in entities if entity]))
+            db.qdoc.update( { "_id": a['_id'] },{"$set": {"entity_ids": unique_entities, "entities": entities, "nlp" : parsed_text } } )
         except:
             err_str = 'No content in article {id}. Deleting article.'.format(id = a['_id'])
             print(err_str)
