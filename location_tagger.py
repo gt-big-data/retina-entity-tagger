7023c93be5afd6ed2807062b45783631a247fc57
Joshua Koh <Joshua@kohfamily.com>
2015-11-16 19:45:40 -0500

    UPdated addLocationid() to find articles with coordinate data and store it in loc field. Probably not working correctly.

--- location_tagger.py
+++ location_tagger.py
@@ -1,52 +1,27 @@
 from dbco import *
 from wikidata import WikidataEntityLookup
 
-# !!!
-# TODO NOTE: A lot of this code may be reuse/redundant. Use at your own discresion/edit freely. :)
-
 wd = WikidataEntityLookup()
-
-
-# Do once: Make 2dsphere index in article collection.
-def initGeotagging():
-    db.qdoc.createIndex( { loc : "2dsphere" } )
-
 
 def getEntitiesNoLocations(limit = 1000):
     return db.qdoc.find( { "$query" : { "loc" : { "$exists" : False } } } ).limit(limit)
+
+def getEntitiesWithGeoLocationData(limit = 1000):
+    return db.qdoc.find( { "$query" : { "properties.GeoLocation" : { "$exists" : True } } } ).limit(limit)
 
 
 # add location to articles in db
 def addLocationId():
     # Entity ID -> Location type -> Location name -> wikidata -> GeoJSON -> qdoc.update
-    articles = getEntitiesNoLocations()
-    tempEntityId
+    bulk = db.qdoc.initialize_unordered_bulk_op()
+    articles = getEntitiesWithGeoLocationData()
     for a in articles:
-        for ent in a["entities"]:
-            tempEntityId = searchEntities(ent)
-            if (getEntity(tempEntityId["id"], ["P625"]) is not None):
-                coordsRaw = propertyLookup(tempEntityId["id"], ["P625"])
-                coordsJSON = coordsToJSON(coordsRaw)
-                # Convert to GeoJSON
-                # Update db
-                db.qdoc.update( { "_id" : a['_id'], { "$set": { "loc" : coordsJSON } } )
+        lat = a["properties"]["GeoLocation"]["params"]["latitude"]
+        long = a["properties"]["GeoLocation"]["params"]["longitude"]
+        coords = coordsToJSON(lat, long)
+        bulk.find( { a["loc"] : { "$exists" : False } } ).update( { "$set" :  { "loc" : coords } } )
+    bulk.execute()
 
-def coordsToJSON(coords):
+def coordsToJSON(lat, long):
     # TODO Use of coords is probably very wrong
-    return { type: "Point", coordinates: [ coords ] }
-
-
-# UPDATING ARTICLES
-# Setting: Run once/when stored articles do not have locations and need them
-# Get all articles from collection w/o locations
-# Add loc field to articles
-# Make geoJSON object with article's lat and long
-# db.collection.insert( geoJSOn data )
-# Bulk push articles back to collection
-
-# FOR NEW ARTICLES
-# Setting: Do this after tagging with entities, before pushing to collection
-# Add loc field to articles
-# Make geoJSON object with article's lat and long
-# db.collection.insert( geoJSOn data )
-# Push article to collection as normal
+    return { type: "Point", coordinates: [ lat, long ] }
